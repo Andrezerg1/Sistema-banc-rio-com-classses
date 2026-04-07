dd7f72b6792fa8b21cdf7613eeb81a4f9bae1893
André Zerger <160751033+Andrezerg1@users.noreply.github.com>
2026-04-07 17:17:25 -0300

    Update contas.py

--- contas.py
+++ contas.py
@@ -2,25 +2,26 @@ import abc
 
 #Classe abstrata que herda do módulo
 class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo=0):
+    def __init__(self, agencia: int, conta: int, saldo: float =0):
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
 
     #Torna o método obrigatório nas subclassses
     @abc.abstractmethod
-    def sacar(self, valor_saque):
+    def sacar(self, valor_saque: float) -> float:
         self.saldo -= valor_saque
 
-    def depositar(self, valor_deposito):
+    def depositar(self, valor_deposito: float) -> float:
         self.saldo += valor_deposito
         self.detalhes(f'(Depósito: {valor_deposito})')
+        return self.saldo
 
-    def detalhes(self, msg=''):
+    def detalhes(self, msg: str =''):
         print(f'O seu saldo é {self.saldo:.2f} {msg}.')
 
 class ContaPoupança(Conta):
-    def sacar(self, valor_saque):
+    def sacar(self, valor_saque: float):
         valor_pos_saque = self.saldo - valor_saque
         
         if valor_pos_saque >= 0:
@@ -30,9 +31,10 @@ class ContaPoupança(Conta):
         else:
             print('Você não têm esse valor disponível para saque.')
             self.detalhes(f'Saque negado: {valor_saque}')
+            return self.saldo
 
 class ContaCorrente(Conta):
-    def __init__(self, agencia, conta, saldo=0, limite=0):
+    def __init__(self, agencia: int, conta: int, saldo: float =0, limite: float =0):
         super().__init__(agencia, conta, saldo)
         self.limite = limite
 
@@ -47,18 +49,13 @@ class ContaCorrente(Conta):
         else:
             print(f'Seu limite é: {self.limite:.2f}')
             self.detalhes(f'Saque negado: {valor_saque}')
+            return self.saldo
 
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'({self.agencia!r}, {self.conta!r}, {self.saldo!r}, {self.limite!r})'
+        return f'{class_name}{attrs}'        
 
 
 if __name__ == '__main__':
-    #cp1 = ContaPoupança('0100-7', 1, 1500)
-    #cp1.sacar(0)
-    #cp1.depositar(100)
-    #cp1.sacar(1600)
-
-    #print('###')
-
-    cc1 = ContaCorrente('0100-7', 1, 1500, 200)
-    cc1.sacar(1800)
-    #cc1.depositar(100)
-    #cc1.sacar(1600)
+    ...
